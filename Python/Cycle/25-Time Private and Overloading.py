a81f45fe2a31e06670b7e11c0870b8c837eb5eb1
Trans MIC <transusemailmic@gmail.com>
2025-10-23 13:34:14 +0530

    23-10-2025

--- Python/Cycle/25-Time Private and Overloading.py
+++ Python/Cycle/25-Time Private and Overloading.py
@@ -2,11 +2,24 @@
 
 class Time:
     def __init__(self,hour,minute,second):
-        self.hour=hour
-        self.minute=minute
-        self.second=second
+        self.__hour=hour
+        self.__minute=minute
+        self.__second=second
+    def __add__(self,other):
+        s=self.__second+other.__second
+        m=self.__minute+other.__minute
+        h=(self.__hour+other.__hour)%24
+        if s>60:
+            s=s%60
+            m=m+1
+        if m>60:
+            m=m%60
+            h=h+1
+        return Time(h,m,s)    
+        
     def __str__(self):
-        return "Time: %02d:%02d:%02d" %(self.hour,self.minute,self.second)
+        return "Time: %02d:%02d:%02d" %(self.__hour,self.__minute,self.__second)
 
-tim=Time(12,1,23)
-print(tim)
+tim1=Time(12,1,23)
+tim2=Time(1,2,4)
+print(tim1+tim2)
